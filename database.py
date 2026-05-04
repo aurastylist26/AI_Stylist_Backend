7d1a2c471ba9801b12422927431c2fe0eb530071
Soumya Pawar <soumya.pawar2605@gmail.com>
2026-05-04 18:32:03 +0530

    Hardcoded Mongo URL for testing

--- database.py
+++ database.py
@@ -1,12 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-MONGO_URL = os.getenv("MONGO_URL")
+# We are bypassing the environment variables completely for this test!
+# PASTE YOUR EXACT MONGODB LINK INSIDE THESE QUOTES:
+MONGO_URL = "mongodb+srv://officialaurastylist26:aurastylist123@cluster26.bohww7c.mongodb.net/aura_saas?retryWrites=true&w=majority"
 
 client = AsyncIOMotorClient(MONGO_URL)
-
 db = client.aura_saas
 users_collection = db.users
