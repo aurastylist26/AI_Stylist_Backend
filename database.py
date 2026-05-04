00a8a51f81323e85fdb1ca140d0045b41503e297
Soumya Pawar <soumya.pawar2605@gmail.com>
2026-05-04 17:15:07 +0530

    Added missing database.py file

---
+++ database.py
@@ -0,0 +1,12 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = AsyncIOMotorClient(MONGO_URL)
+
+db = client.aura_saas
+users_collection = db.users
