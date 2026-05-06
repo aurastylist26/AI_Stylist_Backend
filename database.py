105ee8119af85f073c2be07be94b7d4dc07e54f1
Soumya Pawar <soumya.pawar2605@gmail.com>
2026-05-06 12:56:48 +0530

    Added missing quotes to database URL

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-# We are bypassing the environment variables completely for this test!
-# PASTE YOUR EXACT MONGODB LINK INSIDE THESE QUOTES:
+# Notice the quotation marks at the very beginning and very end of the link!
 MONGO_URL = "mongodb+srv://officialaurastylist26:aurastylist123@cluster26.bohww7c.mongodb.net/aura_saas?retryWrites=true&w=majority"
 
-client = AsyncIOMotorClient(mongodb+srv://officialaurastylist26:aurastylist123@cluster26.bohww7c.mongodb.net/aura_saas?retryWrites=true&w=majority)
+client = AsyncIOMotorClient(MONGO_URL)
 db = client.aura_saas
 users_collection = db.users
